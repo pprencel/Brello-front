2e5dd6194b96bd98449afb2a7eea38ed808addd9
Przemysław Prencel <przemyslaw.prencel@goodylabs.com>
2019-10-21 20:43:08 +0200

    cors test on server

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,17 +1,23 @@
 import axios from 'axios';
 import api from '../constants/api'
-console.log(api);
+
 // const tmpToken = 'v6VdyELtR1ip5qiNGW9jLojW';
 const axiosInstance = axios.create({
   baseURL: api.TEST,
-  // headers: { TOKEN: tmpToken }
+  headers: {
+    // TOKEN: tmpToken,
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+    'Accept': '*/*'
+  }
 });
 
 axiosInstance.interceptors.response.use(
   response => response,
   error => {
     console.log(error);
-    return Promise.reject(error.response.data);
+    return Promise.reject(error);
   }
 );
 
