93b8bd96ea5b9f63c00c60f21ae9306659444415
Przemysław Prencel <hartag96@gmail.com>
2019-11-27 19:21:39 +0100

    prevent input autocomplete

--- src/containers/SignIn.svelte
+++ src/containers/SignIn.svelte
@@ -8,15 +8,15 @@
 </script>
 
 <div class="flex flex-col justify-center items-center text-xl m-10">
-  <form on:submit|preventDefault="{handleSubmit}" class="bg-indigo-400 p-4" style="width: 300px">
+  <form on:submit|preventDefault="{handleSubmit}" class="bg-indigo-400 p-4" style="width: 300px" autocomplete="off">
     <label class="block w-full" for="email">Email / Login</label>
-    <input class="block w-full mt-1 p-1" type="text" id="username">
+    <input class="block w-full mt-1 p-1" type="text" id="username" autocomplete="false">
     <label class="block w-full mt-3" for="email">Password</label>
-    <input class="block w-full mt-1 p-1" type="password" id="password">
+    <input class="block w-full mt-1 p-1" type="password" id="password" autocomplete="false">
     <button class="block border block w-2/3 mx-auto mt-5 border-black" type="submit">Sign in</button>
   </form>
 </div>
-<!-- 
+<!--
 <div class="flex flex-col justify-center items-center text-xl m-10">
   <form on:submit|preventDefault="{handleSubmit}" class="bg-indigo-400 p-4" style="width: 300px">
     <label class="block w-full" for="email">Email / Login</label>
--- src/containers/SignUp.svelte
+++ src/containers/SignUp.svelte
@@ -9,15 +9,15 @@
   }
 </script>
 <div class="flex flex-col justify-center items-center text-xl m-10">
-  <form on:submit|preventDefault="{handleSubmit}" class="bg-indigo-400 p-4" style="width: 300px">
+  <form on:submit|preventDefault="{handleSubmit}" class="bg-indigo-400 p-4" style="width: 300px" autocomplete="off">
     <label class="block w-full" for="email">Email</label>
-    <input class="block w-full mt-1 p-1" type="text" id="email">
+    <input class="block w-full mt-1 p-1" type="text" id="email" autocomplete="false">
     <label class="block w-full" for="login">Login</label>
-    <input class="block w-full mt-1 p-1" type="text" id="login">
+    <input class="block w-full mt-1 p-1" type="text" id="login" autocomplete="false">
     <label class="block w-full mt-3" for="email">Password</label>
-    <input class="block w-full mt-1 p-1" type="password" id="password">
+    <input class="block w-full mt-1 p-1" type="password" id="password" autocomplete="false">
     <label class="block w-full mt-3" for="email">Password confirmation</label>
-    <input class="block w-full mt-1 p-1" type="password" id="password_confirmation">
+    <input class="block w-full mt-1 p-1" type="password" id="password_confirmation" autocomplete="false">
     <button class="block border block w-2/3 mx-auto mt-5 border-black" type="submit">Create account</button>
   </form>
 </div>
